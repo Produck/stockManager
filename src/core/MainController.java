d5e70c19aff4f2113c9a26e8c61f144bd9a594ea
sykim <suyoung154@gmail.com>
2018-03-07 09:13:27 +0900

    Remove Filechooser, easy to use

--- src/core/MainController.java
+++ src/core/MainController.java
@@ -62,34 +62,17 @@ public class MainController {
         btnCodeGeneration.setOnMouseClicked(event -> {
             addLog("코드생성 결과물 저장 위치 선택 중...");
             // 파일이 없을시 취소
-            if (!validate()) return;
+            if (validate()) return;
 
             String origin = txtOriginFilePath.getText();
             String target = txtStockFilePath.getText();
 
-            // 파일선택
-            fileChooser.setTitle("생성 결과물 위치");
-            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Excel (*.xlsx)","*.xlsx"));
-            File fileToSave = fileChooser.showSaveDialog(mainPane.getScene().getWindow());
-
-            // 결과에 따른 로그
-            if (fileToSave == null) {
-                addLog("위치 선택이 취소됨.");
-                return;
-            }
-
-            if (!fileToSave.getName().contains("."))
-                fileToSave = new File(fileToSave.getAbsolutePath() + ".xlsx");
-
-            addLog("위치 선택 : [" + fileToSave.getName() + "]");
-
             addLog("원본파일로부터 코드 추출 시작...");
 
-            File finalFileToSave = fileToSave;
             Thread thread = new Thread(() -> {
                 try {
                     Map<String, String> nameToCode = excelManager.getCodeMapCodeAsValue(origin);
-                    excelManager.writeCodeFile(nameToCode, target, finalFileToSave.getAbsolutePath());
+                    excelManager.writeCodeFile(nameToCode, target);
                     addLog("코드 추출 완료");
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -104,7 +87,7 @@ public class MainController {
             addLog("반영재고 결과물 저장 위치 선택 중...");
 
             // 파일이 없을시 취소
-            if (!validate()) return;
+            if (validate()) return;
 
             String origin = txtOriginFilePath.getText();
             String target = txtStockFilePath.getText();
@@ -137,20 +120,21 @@ public class MainController {
         });
     }
 
-    @FXML public void addLog(String log) {
+    @FXML
+    private void addLog(String log) {
         txtAreaLog.appendText("\t" + log + "\n");
     }
 
-    public boolean validate() {
+    private boolean validate() {
         // 파일 확인
         if (txtStockFilePath.getText() == null || txtStockFilePath.getText().trim().isEmpty()) {
             addLog("쇼핑몰재고 파일이 없습니다.");
-            return false;
+            return true;
         } else if (txtOriginFilePath.getText() == null || txtOriginFilePath.getText().trim().isEmpty()) {
             addLog("재고현황원본 파일이 없습니다.");
-            return false;
+            return true;
         }
 
-        return true;
+        return false;
     }
 }
